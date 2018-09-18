74cc77d6ac1fb589be517de6f34a263eb4424047
ywj <ywj@ywj.cn>
2018-09-18 23:30:23 +0800

    eureka集群3

---
+++ eureka3/src/main/java/cn/ywj/eureka/Eureka3Application.java
@@ -0,0 +1,14 @@
+package cn.ywj.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eureka3Application.class, args);
+	}
+}
